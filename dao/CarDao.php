cfcc3eae5a0d92003fdf9a2867ddb8254744f329
Eduardo Neves <ematheusneves13@gmail.com>
2022-08-23 12:21:47 -0300

    Fatorando cod

--- dao/CarDao.php
+++ dao/CarDao.php
@@ -4,6 +4,12 @@ include_once("models/Car.php");
 
 class CarDAO implements CarDAOInterface {
 
-    
+    public function findAll() {
+
+    }
+
+    public function create(car $car) {
+        
+    }
 
 }
