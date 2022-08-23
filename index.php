c07f8b2aa85ae1c8eae2250dedad966fd20a3426
Eduardo Neves <ematheusneves13@gmail.com>
2022-08-23 12:27:39 -0300

    Criado formulario para obter dados do usuario e enviar os dadospara o banco

--- index.php
+++ index.php
@@ -3,3 +3,18 @@
 include_once("db.php");
 
 ?>
+
+<h1>Insira um carro</h1>
+<form action="process.php" method="POST">
+
+    <label for="brand">Marca do carro:</label>
+    <input type="text" name="brand" placeholder="Insira a marca do carro">
+
+    <label for="km">Insira a kilometragem:</label>
+    <input type="text" name="km" placeholder="Insira a kilometragem">
+
+    <label for="color">Cor do carro:</label>
+    <input type="text" name="color" placeholder="Insira a cor do carro">
+
+
+</form>
